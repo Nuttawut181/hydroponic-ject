3d7f6c65ab6e75db54f6c9f4a7ec05851d51f388
nuttawut <painf181@gmail.com>
2025-05-10 21:26:13 +0700

    add route

--- server/app.py
+++ server/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/<Ligh>/<temp>/<tds>")
+def get_light(light,temp,tds):
+    return f"Light:{light} lux. {temp} c {tds} ppm"
